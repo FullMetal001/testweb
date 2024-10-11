24802eb9f9ed0d9bee874c2bd597a803823f7be5
github_FullMetal001 <tantun_gel@hotmail.com>
2024-10-11 12:37:24 -0400

    3 commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Esto es una prueba</h1>
+      <h1>Vita + React 2024</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
