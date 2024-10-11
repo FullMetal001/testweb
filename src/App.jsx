5d47d91e2920f049defb00848eda491a9e2aef59
github_FullMetal001 <tantun_gel@hotmail.com>
2024-10-11 13:29:01 -0400

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           la cuenta es {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit2 <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
